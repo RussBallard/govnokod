23fa3ebd00418c39a91ee6ed0d124db245c55b10
RussBallard <41388123+RussBallard@users.noreply.github.com>
2019-10-12 18:45:07 +0400

    Initial commit

---
+++ test/wildberries/pipelines.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exporters import JsonItemExporter
+
+
+import codecs
+import json
+
+
+class WildberriesPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('items.json', mode = 'w', encoding = 'utf-8')
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line.decode('unicode_escape'))
+        return item
+
